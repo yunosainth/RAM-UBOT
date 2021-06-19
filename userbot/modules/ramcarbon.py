8f4435748447e984788a434eb19369da1fd78799
ramadhan73 <81619894+ramadhan73@users.noreply.github.com>
2021-06-19 14:07:32 +0000

    Update ramcarbon.py

--- userbot/modules/ramcarbon.py
+++ userbot/modules/ramcarbon.py
@@ -79,8 +79,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created by [ラマダニ。](https://t.me/maafgausahsokap/)\
-        \nGroup [SPAM BOT](https://t.me/ootspambot/)",
+        caption="Created by [ラマダニ。](https://t.me/gksukaribett/)\
+        \nChannel [SPAM BOT](https://t.me/calonpenyanyi/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -145,8 +145,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="created by [ラマダニ。](https://t.me/maafgausahsokap/)\
-        \nGroup Support [Dawn Labs](https://t.me/ootspambot/)",
+        caption="created by [ラマダニ。](https://t.me/gksukaribett/)\
+        \nChannel Support [Dawn Labs](https://t.me/calonpenyanyi/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -211,8 +211,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created By [ラマダニ。](https://t.me/maafgausahsokap/)\
-        \nGroup Support [Spam](https://t.me/ootspambot/)",
+        caption="Created By [ラマダニ。](https://t.me/gksukaribett/)\
+        \nChannel Support [Spam](https://t.me/calonpenyanyi/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -277,8 +277,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created by [ラマダニ。](https://t.me/maafgausahsokap/),\
-        \nGroup Support [Group Spam](https://t.me/ootspambot)",
+        caption="Created by [ラマダニ。](https://t.me/gksukaribett/),\
+        \nChannel Support [Group Spam](https://t.me/calonpenyanyi)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -343,8 +343,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created by [ラマダニ。](https://t.me/maafgausahsokap/),\
-        \nGroup Support [Spam](https://t.me/ootspambot/)",
+        caption="Created by [ラマダニ。](https://t.me/gksukaribett/),\
+        \nChannel Support [Spam](https://t.me/calonpenyanyi/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
