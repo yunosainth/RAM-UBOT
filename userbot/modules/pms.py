9d7a03759c1a893e26ccb5bcee22b3976bd8431f
ramadhan73 <81619894+ramadhan73@users.noreply.github.com>
2021-07-12 13:29:30 +0000

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -33,9 +33,9 @@ DEF_UNAPPROVED_MSG = (
     f"**ANAK KONTOL ANAK NGENTOT,KALO NGECHAT MAJIKAN {DEFAULTUSER} ITU SALAM,\nHABIS ITU SABAR TUNGGU MAJIKAN GUA BALES,\nKALO GA DI BALES - BALES, LU JANGAN NYEPAM KONTOL, APA LAGI LU NGECHAT NYA CUMA MINTA VCS, BISA GUA BLOKIR!! KALO NYEPAM JUGA TAR GUA BLOKIR!!!! TUNGGU SI {DEFAULTUSER} NERIMA PESAN LU**\n"
     "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰\n"
     "╭✠╼━━━━━━❖━━━━━━━✠╮\n"
-    "┣[• MASUK AJA KE @TEMAN_RANDOM\n"
+    "┣[• MASUK AJA KE @calonpenyanyi\n"
     "┣[• PESAN OTOMATIS\n"
-    "┣[• BY RAM-UBOT\n"
+    "┣[• BY DHAN-UBOT\n"
     "╰✠╼━━━━━━❖━━━━━━━✠╯")
 # =================================================================
 
