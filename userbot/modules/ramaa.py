407fd191da9565a05bac21c7058d2c5d7ad64a59
ramadhan73 <81619894+ramadhan73@users.noreply.github.com>
2021-11-07 13:09:55 +0700

    Update ramaa.py

--- userbot/modules/ramaa.py
+++ userbot/modules/ramaa.py
@@ -28,32 +28,32 @@ async def typewriter(typew):
                      "\n**Permisi Aku mau nimbrung Kk..**")
 
 
-@register(outgoing=True, pattern='^.nyanyi(?: |$)(.*)')
+@register(outgoing=True, pattern='^.dhan(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("**POTONG**")
-    await typew.edit("**BEBEK ANGSA**")
+    await typew.edit("**RAMA**")
+    await typew.edit("**PINTER**")
     sleep(1)
-    await typew.edit("**MASAK DI**")
-    await typew.edit("**KUALIIIII**")
+    await typew.edit("**KALIAN**")
+    await typew.edit("**ENGGA**")
     sleep(2)
-    await typew.edit("**DEKET UDAH LAMA**")
-    await typew.edit("TAPI BELOM PASTIIII**")
+    await typew.edit("**RAMA IMUT**")
+    await typew.edit("KALIAN ENGGA**")
     sleep(2)
-    await typew.edit("**SORONG KE KIRIII**")
-    await typew.edit("**SORONG KE KANANN**")
+    await typew.edit("**RAMA CERDAS**")
+    await typew.edit("**KALIAN ENGGA**")
     sleep(2)
-    await typew.edit("**LALALALA**")
-    await typew.edit("**LALALALA TAIIIII**")
+    await typew.edit("**RAMA WARAS**")
+    await typew.edit("**KALIAN ENGGA**")
     sleep(2)
-    await typew.edit("**MENULIS TUGAS DI ATAS KERTAS**")
-    await typew.edit("**SELESAI ITU KU MASUKAN TAS**")
+    await typew.edit("**TAPI RAMA**")
+    await typew.edit("**SUKA DI GHOTING**")
     sleep(2)
-    await typew.edit("**AKU KIRA AKU PRIORITAS**")
-    await typew.edit("**TERNYATA ADA YANG LEBIH PANTAS**")
+    await typew.edit("**SAMA**")
+    await typew.edit("**CEWE CEWE**")
     sleep(2)
-    await typew.edit("**MUKA LU JELEK**")
-    await typew.edit("**GK USAH SOK GHOSTING ORANG**")
+    await typew.edit("**RAMA TERUS KAN? **")
+    await typew.edit("**IYALAH KAN BOT RAMA**")
     sleep(3)
     await typew.edit("**XIXIXIXIXI**")
 
@@ -65,6 +65,6 @@ CMD_HELP.update({
     \nUsage: menampilkan alive bot.\
     \n\n`.sadboy`\
     \nUsage: hiks\
-    \n\n`.punten` ; `.nyanyi`\
+    \n\n`.punten` ; `.dhan`\
     \nUsage: misi."
 })
